ad676267838f9a0b229673b93436537a3e0f564c
xiaoli <xiaoli@qq.com>
2023-05-13 14:36:20 +0800

    初始化主组件

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,12 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html {
+   font-size: 20px;
+   background-color: #DDDDDD
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+   margin: 0;
+   padding: 0;
 }
 </style>
